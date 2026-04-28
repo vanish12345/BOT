4a4667c7d2717edc55b848a9bc3c01600056dc6e
vanish12345 <144611141+vanish12345@users.noreply.github.com>
2026-04-28 17:46:11 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -153,5 +153,5 @@ def vk_webhook():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
